0e8170fe7ccbd5f40b0ee995ca4916016c0b1c51
cdachariom <guptahari96@gmail.com>
2023-11-18 22:46:36 +0530

    pattern

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 public class Hello{
     public static void main(String[]args){
-        System.out.println("Hello World!");
+        System.out.println(" Star Pattern");
+        for (int i=0;i<5;i++){
+            for(int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
